edad1761d4a21cb90717766734d210b0d6e7f507
Tim Poffenberger <timpoffenberger23@gmail.com>
2023-10-13 20:01:23 -0500

    minor file structuring

---
+++ assets/scripts/script.js
@@ -0,0 +1,4 @@
+// api token 9dfb3982f4e44cee088dc829481cd269
+// 5day 3hour api api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}
+
+// geocoding api http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit={limit}&appid={API key}
